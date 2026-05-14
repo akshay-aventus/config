30ecebe863a0f59d7d6210b6cce333979a5d41b0
Akshay KM <akshay@aventusinformatics.in>
2026-05-14 18:18:22 +0530

    some options changes

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -6,10 +6,13 @@ vim.opt.termguicolors = true
 vim.o.relativenumber = true
 vim.o.number = true
 
-vim.opt.undodir = os.getenv("~/.vim/undodar")
+vim.opt.undodir = os.getenv("~/.vim/undodir")
 vim.opt.undofile = true
 
-vim.opt.swapfile = false
+vim.opt.directory = vim.fn.stdpath("state") .. "/swap//"
+vim.opt.updatecount = 100
+
+vim.opt.swapfile = true
 vim.opt.backup = false
 
 vim.o.splitright = true
@@ -22,17 +25,6 @@ vim.opt.wrap = false
 vim.opt.colorcolumn = "80"
 
 vim.opt.cursorline = true
-vim.diagnostic.config({
-	update_in_insert = true,
-	severity_sort = true,
-	float = { source = "if_many" },
-	underline = { severity = vim.diagnostic.severity.ERROR },
-
-	virtual_text = false,
-	virtual_lines = false,
-
-	jump = { float = true },
-})
 
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
@@ -41,8 +33,6 @@ vim.opt.expandtab = false
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 8
 vim.opt.smartindent = true
-
-vim.opt.updatetime = 50
 
 vim.opt.statusline = "%f%h%m%r%w%=  %y %l,%c | %P | %{strftime('%H:%M')}"
 
